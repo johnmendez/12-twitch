1b3096818d980d526be7dbbb7bbe99ab119c5ce2
johnmendez <john.a.mendez2012@gmail.com>
2017-02-23 14:19:55 -0600

    Basic Html and Css layout

--- app/exercise/add-message-item.js
+++ app/exercise/add-message-item.js
@@ -1,1 +1,5 @@
-export default null;
+import createMessageItem from './create-message-item';
+export default function addMessageItem(item, value) {
+  const messageItem = createMessageItem(value);
+  item.appendChild(messageItem);
+}
--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 import 'whatwg-fetch';
 // Import `data.json`
 // Import `addGamesToList` from `twitch/add-games-to-list`
-
+// import 'data';
+// import 'addGamesToList';
 // Look up a `ul` element with a class called `results`
 // Add games to the results list based on the data from `data.json`

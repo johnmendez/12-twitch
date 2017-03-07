5ee9aa413ba4b58b391bde320d4110f8468f32c3
johnmendez <john.a.mendez2012@gmail.com>
2017-03-07 13:45:00 -0600

    Testing progress

--- app/twitch/create-game-element.js
+++ app/twitch/create-game-element.js
@@ -1,5 +1,11 @@
-export default function() {
+export default function () {
   const el = document.createElement('div');
   el.classList.add('game-item');
+  el.innerHTML = `
+  <div class="frame">
+    <h3 class="game-item__name"></h3>
+  </div>`;
+
+
   return el;
 }

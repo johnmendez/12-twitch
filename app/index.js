4a764183a2f0661042e759c79f927865adffd00b
johnmendez <john.a.mendez2012@gmail.com>
2017-03-07 20:08:13 -0600

    testing passing and exports working

--- app/index.js
+++ app/index.js
@@ -1,7 +1,11 @@
 import 'whatwg-fetch';
+import games from './data';
 // Import `data.json`
-// Import `addGamesToList` from `twitch/add-games-to-list`
-// import 'data';
-// import 'addGamesToList';
+
+import addGamesToList from './twitch/add-games-to-list';
+
 // Look up a `ul` element with a class called `results`
+const grid = document.querySelector('.grid');
+
 // Add games to the results list based on the data from `data.json`
+addGamesToList(grid, games.top);
--- app/twitch/create-game-element.js
+++ app/twitch/create-game-element.js
@@ -1,11 +1,24 @@
-export default function () {
-  const el = document.createElement('div');
+export default function createGameElement(data) {
+  const el = document.createElement('li');
   el.classList.add('game-item');
+  // el.classList.add('grid__item');
+
   el.innerHTML = `
+  <img src="" alt="" class="game-item__pic">
   <div class="frame">
     <h3 class="game-item__name"></h3>
+    <h4 class="game-item__popularity"></h4>
   </div>`;
 
+  const title = el.querySelector('.game-item__name');
+  title.innerText = data.name;
+
+  const popularity = el.querySelector('.game-item__popularity');
+  popularity.innerText = `${data.popularity} Viewers`;
+
+  const pic = el.querySelector('.game-item__pic');
+  pic.src = data.box.large;
+  pic.alt = data.name;
 
   return el;
 }
